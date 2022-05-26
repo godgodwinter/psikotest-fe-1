0180fb59998b4653354b5a74aa19bdd9f57d6f2c
bacanprasetyo <92247271+bacanprasetyo@users.noreply.github.com>
2022-05-26 17:47:26 +0700

    analisa penjurusan

--- src/views/admin/sekolah/submenu/analisapenjurusan/Index.vue
+++ src/views/admin/sekolah/submenu/analisapenjurusan/Index.vue
@@ -153,15 +153,80 @@ const ListTampilkan = ref([
   { label: "Bakat 1", id: "tipe_bakat_1", checked: true },
   { label: "Bakat 2", id: "tipe_bakat_2", checked: false },
   { label: "Bakat 3", id: "tipe_bakat_3", checked: false },
-  { label: "mnt_studi_lanjut_smp", id: "mnt_studi_lanjut_smp", checked: false },
+  { label: "Minat Studi Lanjut SMP", id: "mnt_studi_lanjut_smp", checked: false },
   {
-    label: "Studi Lanjut fakultas",
+    label: "Studi Lanjut SMA/SMK Fakultas 1",
     id: "mnt_studi_lanjut_sma_smk_1_fakultas",
     checked: false,
   },
   {
-    label: "Studi Lanjut prodi",
+    label: "Studi Lanjut SMA/SMK Prodi 1",
     id: "mnt_studi_lanjut_sma_smk_1_prodi",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMA/SMK Fakultas 2",
+    id: "mnt_studi_lanjut_sma_smk_2_fakultas",
+    checked: false,
+  },
+   {
+    label: "Studi Lanjut SMA/SMK Prodi 2",
+    id: "mnt_studi_lanjut_sma_smk_2_prodi",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMA/SMK Kedinasan",
+    id: "mnt_studi_lanjut_sma_smk_kedinasan",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMA",
+    id: "mnt_studi_lanjut_sma",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMK 1",
+    id: "mnt_studi_lanjut_smk_1",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMK 2",
+    id: "mnt_studi_lanjut_smk_2",
+    checked: false,
+  },
+  {
+    label: "Studi Lanjut SMK 3",
+    id: "mnt_studi_lanjut_smk_3",
+    checked: false,
+  },
+  {
+    label: "Sekolah Lanjutan",
+    id: "sekolah_lanjutan",
+    checked: false,
+  },
+  {
+    label: "Sekolah Jurusan",
+    id: "sekolah_jurusan",
+    checked: false,
+  },
+  {
+    label: "Saran Fakultas 1",
+    id: "saran_fakultas_1",
+    checked: false,
+  },
+  {
+    label: "Saran Fakultas 1 Prodi",
+    id: "saran_fakultas_1_prodi",
+    checked: false,
+  },
+  {
+    label: "Saran Fakultas 2",
+    id: "saran_fakultas_2",
+    checked: false,
+  },
+  {
+    label: "Saran Fakultas 2 Prodi",
+    id: "saran_fakultas_2_prodi",
     checked: false,
   },
 ]);
@@ -200,7 +265,7 @@ const onToggleList = (index) => {
     <div>
       <span
         class="text-2xl sm:text-3xl leading-none font-bold text-gray-700 shadow-sm"
-        >Analis Penjurusan</span
+        >Analisa Penjurusan</span
       >
     </div>
     <div class="md:py-0 py-4 space-x-2 space-y-2"></div>
