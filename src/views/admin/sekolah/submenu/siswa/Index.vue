3baa3e5851c3535e2e5e6870f9d9bac2bd69cd30
godgodwinter <kakadz.pro@gmail.com>
2022-06-10 16:22:44 +0700

    update tw

--- src/views/admin/sekolah/submenu/siswa/Index.vue
+++ src/views/admin/sekolah/submenu/siswa/Index.vue
@@ -108,12 +108,12 @@ const columns = [
   },
   {
     label: "Username Ortu",
-    field: "username",
+    field: "ortu_username",
     type: "String",
   },
   {
     label: "Password Default Ortu",
-    field: "passworddefaultortu",
+    field: "ortu_passworddefault",
     type: "String",
   },
 ];
@@ -302,6 +302,7 @@ const doCopyClipboard = (item) => {
                 <div class="flex justify-center gap-2">
                   <div class="text-center">{{ props.row.username }}</div>
                   <span
+                    v-if="props.row.username"
                     @click="doCopyClipboard(props.row.username)"
                     class="hover:text-primary cursor-pointer"
                   >
@@ -326,7 +327,60 @@ const doCopyClipboard = (item) => {
                 <div class="flex justify-center gap-2">
                   <div class="text-center">{{ props.row.passworddefault }}</div>
                   <span
+                    v-if="props.row.passworddefault"
                     @click="doCopyClipboard(props.row.passworddefault)"
+                    class="hover:text-primary cursor-pointer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </span>
+              <span v-else-if="props.column.field == 'ortu_username'">
+                <div class="flex justify-center gap-2">
+                  <div class="text-center">{{ props.row.ortu_username }}</div>
+                  <span
+                    v-if="props.row.ortu_username"
+                    @click="doCopyClipboard(props.row.ortu_username)"
+                    class="hover:text-primary cursor-pointer"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-6 w-6"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      stroke-width="2"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"
+                      />
+                    </svg>
+                  </span>
+                </div>
+              </span>
+              <span v-else-if="props.column.field == 'ortu_passworddefault'">
+                <div class="flex justify-center gap-2">
+                  <div class="text-center">
+                    {{ props.row.ortu_passworddefault }}
+                  </div>
+                  <span
+                    v-if="props.row.ortu_passworddefault"
+                    @click="doCopyClipboard(props.row.ortu_passworddefault)"
                     class="hover:text-primary cursor-pointer"
                   >
                     <svg
